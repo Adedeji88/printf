0909776f6fcb11954394c09d27b5ac660745ca8f
Adedeji88 <mustaphadedeji@yahoo.com>
2022-10-16 19:58:17 -0700

    Added main.h

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+* struct intfce - interface for printing
+* @id: the character, which identifies the type of arg
+* @func: function to run if id matches
+*/
+typedef struct intfce
+{
+	char id;
+	int (*func)(va_list);
+} interface;
+
+int _printf(const char *format, ...);
+int _print_char(va_list ls);
+int _print_mod(va_list ls);
+int _print_string(va_list ls);
+int _print_int(va_list ls);
+
+#endif /* MAIN */
